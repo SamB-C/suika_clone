33add569c79247aaff529936e93d886342552cd2
Olly Harding <oliverdharding@gmail.com>
2024-12-21 16:58:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
 import pygame
+import sys
+
 pygame.init()
 
 # Sets width and height of the screen
@@ -12,22 +13,19 @@ screen = pygame.display.set_mode(size)
 
 # Loads the image
 radius = 20
-ballrect = pygame.Rect(width // 2 - radius, height //
-                       2 - radius, radius * 2, radius * 2)
+ballrect = pygame.Rect(width // 2 - radius, height // 2 - radius, radius * 2, radius * 2)
 
 # Box
-
-wallleft = pygame.surface.Surface(screen, (0, 0, 10, height))
+wallleft = pygame.Surface((10, height))
 wallleft.fill((255, 255, 255))
-wallright = pygame.surface.Surface(screen, (width - 10, 0, 10, height))
+wallright = pygame.Surface((10, height))
 wallright.fill((255, 255, 255))
-wallbottom = pygame.surface.Surface(screen, (0, height - 10, width, 10))
+wallbottom = pygame.Surface((width, 10))
 wallbottom.fill((255, 255, 255))
 
-wallleftrect = wallleft.get_rect()
-wallrightrect = wallright.get_rect()
-wallbottomrect = wallbottom.get_rect()
-
+wallleftrect = wallleft.get_rect(topleft=(0, 0))
+wallrightrect = wallright.get_rect(topright=(width, 0))
+wallbottomrect = wallbottom.get_rect(bottomleft=(0, height))
 
 # Main loop
 while True:
@@ -38,8 +36,6 @@ while True:
 
     # Moves the ball
     ballrect = ballrect.move(speed)
-
-    # Logic for ball to bounce
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
@@ -47,13 +43,11 @@ while True:
 
     # Fills the screen with black
     screen.fill(black)
-
-    # Draws the ball
-    screen.blit(ball, ballrect)
+    # Draws the ball and walls
+    pygame.draw(circle, ballrect)
     screen.blit(wallleft, wallleftrect)
     screen.blit(wallright, wallrightrect)
     screen.blit(wallbottom, wallbottomrect)
 
-    pygame.draw.circle(screen, (255, 255, 255), ballrect.center, radius)
     # Updates the screen
     pygame.display.flip()
