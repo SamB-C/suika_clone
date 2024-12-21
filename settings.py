5144ae0406b69ef7d7aeb3b04f7bd0b5f9af4d88
SamB-C <samson_bc@icloud.com>
2024-12-21 20:03:46 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,9 @@
+# Add values that you consistently change when debugging to the settings.py file.
+# This file is exempt from git, so any changes to it aren't committed.
+
+# This should reduce the number of merge conflicts,
+# but bear in mind that your settings are therefore local and not shared to other developers,
+# so may explain any discrepency in output.
+
 balls_to_create = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 fps = 60
