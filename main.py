816f0163296762b78036c3d33de18ed576c15e82
SavageJoe677 <145936948+SavageJoe677@users.noreply.github.com>
2024-12-21 17:14:11 +0000

    Attempt to add gravity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 import pygame
+
+import ballmotion
+
 pygame.init()
 
 # Sets width and height of the screen
@@ -9,6 +12,13 @@ black = 0, 0, 0
 
 # Creates a screen
 screen = pygame.display.set_mode(size)
+
+# Creates game clock and sets target frames per second
+clock = pygame.time.Clock()
+fps = 60
+
+# Defines acceleration due to gravity
+gravity = 9.8
 
 # Loads the image
 ball = pygame.image.load("intro_ball.gif")
@@ -21,12 +31,18 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
+
+
     # Moves the ball
     ballrect = ballrect.move(speed)
+    print(ballrect.bottom, speed[1])
     if ballrect.left < 0 or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
+
+    # Accelerates ball
+    speed = ballmotion.calc_speed(speed, gravity, fps)
 
     # Fills the screen with black
     screen.fill(black)
@@ -34,3 +50,5 @@ while True:
     screen.blit(ball, ballrect)
     # Updates the screen
     pygame.display.flip()
+    # Waits for next frame
+    clock.tick(fps)
